6519e34d12b28508f5f6eb47d4750a09c3877748
Hyeonwoo Jeon <medianhw@gmail.com>
2018-10-05 11:12:28 +0900

    add navigator files

---
+++ components/AppTapNavigator/AddMediaTab.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+export default class AddMediaTab extends Component {
+  render() {
+    return (
+      <View>
+        <Text> AddMediaTab </Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({})
---
+++ components/AppTapNavigator/ProfileTab.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+export default class ProfileTab extends Component {
+  render() {
+    return (
+      <View>
+        <Text> ProfileTab </Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({})
