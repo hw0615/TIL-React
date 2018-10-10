789c2e07557c7ab59c29239a39243ffc61492faf
Hyeonwoo Jeon <medianhw@gmail.com>
2018-10-10 15:10:43 +0900

    test cart

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
 import React, {Component} from 'react';
 import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
-import TodoApp from './src/TodoApp'
-import store from './src/store'
-import { Provider } from 'react-redux'
+import ShoppingCart from './ShoppingCart'
+
 export default class App extends React.Component{
 
   render() {
     return (
-      <Provider store={store}>
-        <TodoApp />
-      </Provider>
+      <ShoppingCart />
     )
   }
 }
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux'
-import todos from './todos'
-import visibilityFilter from './visibilityFilter.js'
-
-export default combineReducers({
-  todos,
-  visibilityFilter
-})
